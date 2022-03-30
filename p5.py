90a0e47ab78c9cfa7e62c59489dbee4a4c4c37db
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2022-03-30 06:23:36 +0000

    program 5

---
+++ p5.py
@@ -0,0 +1,19 @@
+# Program to print all the words present in the string and print their length
+s=input('Enter the string ')
+words=s.split()
+for word in words:
+  print(word , ":" ,len(word))
+print("Total words = ",len(words
+
+#FIbonacci series in reverse order
+l=[]
+n=int(input("Enter the number "))
+if n==1:
+  l.append(0)
+else:
+  l.append(0)
+  l.append(1)
+for i in range(2,n):
+  l.append(l[i-1]+l[i-2])
+l.reverse()
+print(l)
